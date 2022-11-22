d79413c93e50443199410d5abcf9ad8db45b41fc
VeronikaFedorenko <ronyaufim@icloud.com>
2022-11-22 20:51:20 +0300

    depth First Traversal

--- lab7/lab7/lab7/lab7.cpp
+++ lab7/lab7/lab7/lab7.cpp
@@ -7,125 +7,92 @@
 #include <locale.h>
 
 
-#include <stdio.h>
-#include <stdlib.h>
-
-struct Node
-{
-    int vertex;
-    struct Node* next;
-};
-
-
-
-struct Graph
-{
-    int numVertices;
-    int* visited;
-    struct Node** adjLists; // we need int** to store a two dimensional array. Similary, we need struct node** to store an array of Linked lists
-};
-
-struct Graph* createGraph(int);
-void addEdge(struct Graph*, int, int);
-void printGraph(struct Graph*);
-void DFS(struct Graph*, int);
-
-
-int main()
-{
-
-    struct Graph* graph = createGraph(4);
-    addEdge(graph, 0, 1);
-    addEdge(graph, 0, 2);
-    addEdge(graph, 1, 2);
-    addEdge(graph, 2, 3);
-
-    printGraph(graph);
-
-    DFS(graph, 2);
-
-    return 0;
+bool checkIsVertertexAdjacent(int** sourceMatrix, int firstVertexToDelite, int secondVertexToDelite) {
+	if (sourceMatrix[firstVertexToDelite][secondVertexToDelite] == 1) {
+		return true;
+	}
+	return false;
 }
 
-void DFS(struct Graph* graph, int vertex) {
-    struct Node* adjList = graph->adjLists[vertex];
-    struct Node* temp = adjList;
+void allocateMatrix(int*** sourceMatrix, int columnCount, int rowsCount) {
 
-    graph->visited[vertex] = 1;
-    printf("Visited %d \n", vertex);
+	*sourceMatrix = (int**)malloc(columnCount * sizeof(int*));
 
-    while (temp != NULL) {
-        int connectedVertex = temp->vertex;
+	for (int i = 0; i < rowsCount; i++)
+	{
+		(*sourceMatrix)[i] = (int*)calloc(rowsCount, sizeof(int));
+	}
+}
 
-        if (graph->visited[connectedVertex] == 0) {
-            DFS(graph, connectedVertex);
-        }
-        temp = temp->next;
-    }
+void fillMatrixRandomElements(int** sourseMatrix, int columnCount, int rowsCount) {
+
+	for (int i = 0; i < rowsCount; i++)
+	{
+		for (int j = 0; j < columnCount; j++)
+		{
+			if (i == j) {
+				sourseMatrix[i][j] = 0;
+			}
+			else
+			{
+				sourseMatrix[i][j] = rand() & 1;
+				sourseMatrix[j][i] = sourseMatrix[i][j];
+
+			}
+			//sourseMatrix[i][j] = rand() % 100 + 1; // заполняем массив случайными числами
+		}
+	}
+}
+
+void printMatrix(int** sourseMatrix, int columnCount, int rowsCount) {
+
+	for (int i = 0; i < rowsCount; i++)
+	{
+		printf("\n");
+		for (int j = 0; j < columnCount; j++)
+		{
+			printf("\t%d", sourseMatrix[i][j]);
+		}
+	}
 }
 
 
-struct Node* createNode(int v)
-{
-    Node* newNode = (Node*) malloc(sizeof(Node));
-    newNode->vertex = v;
-    newNode->next = NULL;
-    return newNode;
+void depthFirstTraversal(int** sourceMatrix, int matrixSize, int* visitedVertex, int vertex) {
+	visitedVertex[vertex] = 1;
+	printf("%d", vertex);
+
+	for (int i = 0; i < matrixSize; i++)
+	{
+		//в текущей вершине есть связь И вершина не посещена
+		if (sourceMatrix[vertex][i] == 1 && visitedVertex[i] == 0) {
+			
+			depthFirstTraversal(sourceMatrix, matrixSize, visitedVertex, i);
+		}
+	}
 }
 
-struct Graph* createGraph(int vertices)
-{
-    struct Graph* graph = (Graph*) malloc(sizeof(struct Graph));
-    graph->numVertices = vertices;
+void main() {
+	srand(time(NULL));
+	setlocale(LC_ALL, "Rus");
+	int** sourceMatrix = NULL;
+	int matrixSize = 0;
+	int* visitedVertexs = NULL;
 
-    graph->adjLists = (Node **) malloc(vertices * sizeof(struct Node*));
+	printf("Введите размер матрицы смежности = ");
+	scanf_s("%d", &matrixSize);
 
-    graph->visited = (int*) malloc(vertices * sizeof(int));
+	visitedVertexs = (int*)calloc(matrixSize, sizeof(int));
 
-    int i;
-    for (i = 0; i < vertices; i++) {
-        graph->adjLists[i] = NULL;
-        graph->visited[i] = 0;
-    }
-    return graph;
+
+	allocateMatrix(&sourceMatrix, matrixSize, matrixSize);
+	fillMatrixRandomElements(sourceMatrix, matrixSize, matrixSize);
+
+	printf("\nИсходная матрица смежности\n");
+
+	printMatrix(sourceMatrix, matrixSize, matrixSize);
+	printf("\n");
+
+	depthFirstTraversal(sourceMatrix, matrixSize, visitedVertexs, 0);
+
+
 }
-
-void addEdge(struct Graph* graph, int src, int dest)
-{
-    // Add edge from src to dest
-    struct Node* newNode = createNode(dest);
-    newNode->next = graph->adjLists[src];
-    graph->adjLists[src] = newNode;
-
-    // Add edge from dest to src
-    newNode = createNode(src);
-    newNode->next = graph->adjLists[dest];
-    graph->adjLists[dest] = newNode;
-}
-
-void printGraph(struct Graph* graph)
-{
-    int v;
-    for (v = 0; v < graph->numVertices; v++)
-    {
-        struct Node* temp = graph->adjLists[v];
-        printf("\n Adjacency list of vertex %d\n ", v);
-        while (temp)
-        {
-            printf("%d -> ", temp->vertex);
-            temp = temp->next;
-        }
-        printf("\n");
-    }
-}
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
